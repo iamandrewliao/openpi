a0e0c7e7c55d9237eef4c311e939c84da2dc6b67
Andrew Liao <55923364+iamandrewliao@users.noreply.github.com>
2025-06-25 01:29:56 -0500

    Changes for pick_green_into_bowl dataset

--- examples/libero/convert_libero_data_to_lerobot.py
+++ examples/libero/convert_libero_data_to_lerobot.py
@@ -24,13 +24,11 @@ from lerobot.common.datasets.lerobot_dataset import LEROBOT_HOME
 from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
 import tensorflow_datasets as tfds
 import tyro
+import numpy as np
 
-REPO_NAME = "your_hf_username/libero"  # Name of the output dataset, also used for the Hugging Face Hub
+REPO_NAME = "iamandrewliao/vla_datasets"  # Name of the output dataset, also used for the Hugging Face Hub
 RAW_DATASET_NAMES = [
-    "libero_10_no_noops",
-    "libero_goal_no_noops",
-    "libero_object_no_noops",
-    "libero_spatial_no_noops",
+    "pick_green_into_bowl",
 ]  # For simplicity we will combine multiple Libero datasets into one training dataset
 
 
@@ -45,28 +43,23 @@ def main(data_dir: str, *, push_to_hub: bool = False):
     # LeRobot assumes that dtype of image data is `image`
     dataset = LeRobotDataset.create(
         repo_id=REPO_NAME,
-        robot_type="panda",
+        robot_type="ur5",
         fps=10,
         features={
             "image": {
                 "dtype": "image",
-                "shape": (256, 256, 3),
-                "names": ["height", "width", "channel"],
-            },
-            "wrist_image": {
-                "dtype": "image",
-                "shape": (256, 256, 3),
+                "shape": (480, 640, 3),
                 "names": ["height", "width", "channel"],
             },
             "state": {
                 "dtype": "float32",
-                "shape": (8,),
-                "names": ["state"],
+                "shape": (6,),
+                "names": ["eef_position, eef_orientation_rpy"],
             },
-            "actions": {
+            "action": {
                 "dtype": "float32",
                 "shape": (7,),
-                "names": ["actions"],
+                "names": ["eef_delta_pose_and_gripper"],
             },
         },
         image_writer_threads=10,
@@ -79,12 +72,14 @@ def main(data_dir: str, *, push_to_hub: bool = False):
         raw_dataset = tfds.load(raw_dataset_name, data_dir=data_dir, split="train")
         for episode in raw_dataset:
             for step in episode["steps"].as_numpy_iterator():
+                eef_pos = step["observation"]["eef_pose"]["position"]
+                eef_rpy = step["observation"]["eef_pose"]["orientation_rpy"]
+                proprio_state = np.concatenate([eef_pos, eef_rpy])
                 dataset.add_frame(
                     {
                         "image": step["observation"]["image"],
-                        "wrist_image": step["observation"]["wrist_image"],
-                        "state": step["observation"]["state"],
-                        "actions": step["action"],
+                        "state": proprio_state,
+                        "action": step["action"],
                     }
                 )
             dataset.save_episode(task=step["language_instruction"].decode())
@@ -95,7 +90,7 @@ def main(data_dir: str, *, push_to_hub: bool = False):
     # Optionally push to the Hugging Face Hub
     if push_to_hub:
         dataset.push_to_hub(
-            tags=["libero", "panda", "rlds"],
+            tags=["pick_and_place", "ur5", "rlds"],
             private=False,
             push_videos=True,
             license="apache-2.0",
